e3ea8d8e4f656baa34e96a4cb956f121cd2264e3
nathan <nathanmanzano@gmail.com>
2023-12-13 18:07:40 +0800

    Make a PHP Router Done

--- controllers/about.php
+++ controllers/about.php
@@ -1,5 +1,5 @@
 <?php
-require 'function.php';
+
 //changes -paul
 
 // second change
--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <?php
 
+
 require 'function.php';
 
-$heading = ' Home';
-
-
-
-
-require "views/index.view.php";
+require 'router.php';
